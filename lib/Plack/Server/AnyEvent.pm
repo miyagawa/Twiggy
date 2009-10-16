c5ed05f5d0f67c4ab4cea49c967345bf2e46ed02
Yuval Kogman <nothingmuch@woobling.org>
2009-10-16 03:20:53 +0200

    shutdown socket in array ref case

--- lib/Plack/Server/AnyEvent.pm
+++ lib/Plack/Server/AnyEvent.pm
@@ -350,7 +350,12 @@ sub _write_body {
 
         if ( ref $body eq 'ARRAY' ) {
             my $buf = join "", @$body;
-            return $self->_write_buf($sock, \$buf);
+            my $done = AE::cv;
+            $self->_write_buf($sock, \$buf)->cb(sub {
+                shutdown $sock, 1;
+                $done->send(1);
+            });
+            return $done;
         } else {
             # flush the output buffer, but not the input buffer
             {
